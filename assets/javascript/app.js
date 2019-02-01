dd50a3668f24ba865a4fd7e3d56dc23578722397
dPaq <dpaquette2.0@gmail.com>
2019-02-01 17:26:46 -0500

    added google books api functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+
+
+    /*
+        Firebase
+        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    */
     var config = {
         apiKey: "AIzaSyCLQQaC2PjCbaoYwkeYea8mlJeNwX-G7Y4",
         authDomain: "project1test-346b5.firebaseapp.com",
@@ -63,7 +69,58 @@ $(document).ready(function () {
 
 
 
+    /*
+        Google Books API
+        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    */
+        // Functions
+        function bookSearch(α) {
+            // TODO: get search string from correct id in html
+            // TODO: append search results to correct place in html
+            α.preventDefault()
+            $('#content').empty()
+            α = $('#search-term').val()
+            $('#search-term').val('')
+            let url = 'https://www.googleapis.com/books/v1/volumes?q=' + α
+            $.get(url).then(function(β) {
+                for (let i in β.items) {
+                    let imgURL = β.items[i].volumeInfo.imageLinks.thumbnail
+                    let γ = $('<div>').addClass('search-image').attr('data-id',β.items[i].id)
+                    $('<img>').attr('src',imgURL).attr('data-id',β.items[i].id).appendTo(γ)
+                    γ.appendTo($('#content'))
+                }
+            })
+        }
+        function bookDetail() {
+            // TODO: append ω data to correct place
+            $('#details').empty()
+            let α = $(this).attr('data-id')
+            let url = 'https://www.googleapis.com/books/v1/volumes/' + α
+            $.get(url).then(function(β) {
+                console.log(β)
+                let ω = {
+                    title: β.volumeInfo.title,
+                    author: β.volumeInfo.authors[0],
+                    rating: β.averageRating,
+                    description: β.volumeInfo.description,  // should be inputted as .html(description)
+                    publisher: β.volumeInfo.publisher,
+                    date: β.volumeInfo.publishedDate,
+                    pages: β.volumeInfo.pageCount,
+                    link: β.volumeInfo.previewLink,
+                    forSale: β.saleInfo,
+                    image: β.volumeInfo.imageLinks.large,
+                }
+                for (let i in ω) {
+                    $('<div>').html(ω[i]).appendTo($('#details'))
+                }
+                return ω
+            })
+        }
 
+        // Listeners
+        // TODO: change/update listener location
+        $(document).on('click','.search',bookSearch)
+        $(document).on('click','.search-image',bookDetail)
 
 
 
